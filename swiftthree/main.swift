f99ae8b29b1b8006456a46579e8539d8d77ef93e
hw20181102929 <51589984+hw20181102929@users.noreply.github.com>
2019-09-25 10:27:30 +0800

    Initial Commit

---
+++ swiftthree/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swiftthree
+//
+//  Created by s20181102929 on 2019/9/25.
+//  Copyright © 2019 s20181102929. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
